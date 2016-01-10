899c7ea8231168618dd24f30c5c68b4f785213c0
Hao Hu <hhu94@uw.edu>
2016-01-10 02:08:59 -0800

    Added bin.

---
+++ bin/cverify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Runs cpplint.py and valgrind for exercises for CSE 333
+
+if ! [[ -s $1 && -s $1.c ]]; then
+  echo "Usage: cverify.sh program [arguments ...]"
+  echo "  Please make sure the compiled program and its .c source file exist."
+  exit 1
+fi
+echo
+echo "Running cpplint.py"
+cpplint.py $1.c
+echo
+echo "Press a key to continue"
+read -n 1 -s
+echo
+echo "Running valgrind"
+valgrind -q --leak-check=full ./$1 "${@:2}"
