9141d61b1fb795694a4fcb47307f47a8cf7ca4f0
Hao Hu <hhu94@uw.edu>
2016-01-29 02:06:30 -0800

    Update to check for .cc files

--- bin/cverify.sh
+++ bin/cverify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Runs cpplint.py and valgrind for exercises for CSE 333
 
-if ! [[ -s $1 && -s $1.c ]]; then
+if ! [[ -s $1 ]] && [[ -s $1.c || -s $1.cc ]]; then
   echo "Usage: cverify.sh program [arguments ...]"
   echo "  Please make sure the compiled program and its .c and .h source" \
          "files exist."
@@ -9,7 +9,7 @@ if ! [[ -s $1 && -s $1.c ]]; then
 fi
 echo
 echo "Running cpplint.py"
-cpplint.py *.c *.h
+cpplint.py *.c *.cc *.h
 echo
 echo "Press a key to continue"
 read -n 1 -s
