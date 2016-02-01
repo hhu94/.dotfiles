4bbc23380938b1adfd25b3ab3aa1e48d03042bd5
Hao Hu <hhu94@uw.edu>
2016-01-31 17:26:33 -0800

    Add rdp config file

--- bin/vdi-cse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ ! ("$#" == 2 && $2 =~ ^[0-9]+$) ]]; then
-  echo >&2 "Usage: ./vdi-cse.sh <username> <vdilab-number>"
-  exit 1
-fi
-
-xfreerdp /d:CSEPCLAB /size:1280x1024 /u:$1 /v:vdilab$2.cs.washington.edu \
-/sec:nla
